db61157ba63cf4bce8ef399549f714e2003e4492
liquanming <864799077@qq.com>
2018-10-18 08:32:29 +0800

    试一试1

--- 1/day03/demo01/demo01/__init__.py
+++
@@ -1,4 +0,0 @@
-import pymysql
-
-#这个方法表示将pymysql当成mysqldb来用
-pymysql.install_as_MySQLdb()
--- 1/day03/demo01/demo01/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-#首页函数
-def index(request):
-	# articles = models.Article.objects.all()
-	return render(request, "blog/index.html", {})
